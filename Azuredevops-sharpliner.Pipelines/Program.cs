080b43277dd60e528757bf02c273d16b4f0f262d
Brian Sheridan <brian.sheridan@gmail.com>
2025-07-25 17:13:11 -0700

    refactor: Remove unused Program.cs file from Azure DevOps pipelines

--- Azuredevops-sharpliner.Pipelines/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sharpliner.AzureDevOps;
-using Sharpliner.AzureDevOps.Tasks;
-
-namespace Azuredevops_sharpliner.Pipelines;
-
-// This file serves as the main entry point for the Pipelines library
-// Pipeline definitions are located in separate files:
-// - DotNetBuildPipeline.cs: Multi-stage build and publish pipeline
-// - DotNetPRPipeline.cs: Single-stage pull request validation pipeline
